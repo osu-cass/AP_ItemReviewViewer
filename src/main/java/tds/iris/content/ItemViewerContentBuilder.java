f55d8e6602b56da4456e5c29e571db8052145b28
suryasrinivas <suryasrinivas2000@gmail.com>
2016-08-02 16:18:55 -0700

    reverted to previous version

--- src/main/java/tds/iris/content/ItemViewerContentBuilder.java
+++ src/main/java/tds/iris/content/ItemViewerContentBuilder.java
@@ -1,22 +1,24 @@
 package tds.iris.content;
 
 import java.io.BufferedOutputStream;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Date;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -24,7 +26,6 @@ import org.springframework.stereotype.Component;
 import AIR.Common.Configuration.AppSettingsHelper;
 import tds.iris.abstractions.repository.ContentException;
 import tds.iris.abstractions.repository.IContentBuilder;
-import tds.iris.rest.ItemController;
 import tds.itempreview.ConfigBuilder;
 import tds.itemrenderer.data.IITSDocument;
 
@@ -33,14 +34,13 @@ import tds.itemrenderer.data.IITSDocument;
 public class ItemViewerContentBuilder implements IContentBuilder {
 	private static final Logger _logger = LoggerFactory.getLogger(ItemViewerContentBuilder.class);
 
-	// private static String GIT_LAB_URL ;
+	 private static String GIT_LAB_URL ;
 	private static String DESTINATION_ZIP_FILE_LOCATION;
 	private static final int BUFFER_SIZE = 4096;
-	// private static final String USER_AGENT = "User-Agent";
-	// private static final String USER_AGENT_VALUE = "Mozilla/5.0 (Windows NT
-	// 6.3; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0";
-	// private static final String REFERRER = "Referer";
-	// private static final String REFERRER_VALUE = "https://www.google.com";
+	 private static final String USER_AGENT = "User-Agent";
+	 private static final String USER_AGENT_VALUE = "Mozilla/5.0 (Windows NT6.3; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0";
+	 private static final String REFERRER = "Referer";
+	 private static final String REFERRER_VALUE = "https://www.google.com";
 	private static final String FILE_EXTENTION = ".zip";
 	private String _contentPath;
 	private ConfigBuilder _directoryScanner = null;
@@ -49,8 +49,6 @@ public class ItemViewerContentBuilder implements IContentBuilder {
 		init();
 	}
 
-	@Autowired
-	ItemController itemController;
 
 	@Override
 	public IITSDocument getITSDocument(String id) throws ContentException {
@@ -60,8 +58,7 @@ public class ItemViewerContentBuilder implements IContentBuilder {
 			String temp = id.substring(6, id.length());
 			String zipFilePath = DESTINATION_ZIP_FILE_LOCATION + temp + FILE_EXTENTION;
 			if (!fileExists(zipFilePath)) {
-				// getItems(new URL(populateUrl(temp)), zipFilePath);
-				getItems("item-187-" + temp, zipFilePath);
+				 getItems(new URL(populateUrl(temp)), zipFilePath);
 			}
 			if (unzip(zipFilePath, DESTINATION_ZIP_FILE_LOCATION)) {
 				_logger.info("Scanning the Directory for the Item Started");
@@ -82,10 +79,10 @@ public class ItemViewerContentBuilder implements IContentBuilder {
 		throw new ContentException(String.format("No content found by id %s", id));
 	}
 
-	// private String populateUrl(String temp) {
-	//
-	// return GIT_LAB_URL+temp;
-	// }
+	 private String populateUrl(String temp) {
+	
+	 return GIT_LAB_URL+temp;
+	 }
 
 	private boolean fileExists(String zipFilePath) {
 
@@ -94,53 +91,29 @@ public class ItemViewerContentBuilder implements IContentBuilder {
 		return f.exists();
 	}
 
-	// private boolean getItems(URL fileURL, String fileSavePath) {
-	// _logger.info("Getting the Item with URL:"+fileURL+" with file
-	// name:"+fileSavePath+" Started");
-	// boolean isSucceed = true;
-	// CloseableHttpClient httpClient = HttpClients.createDefault();
-	// HttpGet httpGet = new HttpGet(fileURL.toString());
-	// httpGet.addHeader(USER_AGENT, USER_AGENT_VALUE);
-	// httpGet.addHeader(REFERRER, REFERRER_VALUE);
-	// try {
-	// CloseableHttpResponse httpResponse = httpClient.execute(httpGet);
-	// HttpEntity fileEntity = httpResponse.getEntity();
-	// if (fileEntity != null) {
-	// FileUtils.copyInputStreamToFile(fileEntity.getContent(), new
-	// File(fileSavePath));
-	// }
-	// } catch (IOException e) {
-	// _logger.error("IO Exception occurred while Getting the Item with
-	// URL:"+fileURL+ e.getMessage());
-	// throw new ContentException(String.format("UnKnown Exception Occurred
-	// while getting item ID: %s",e.getMessage()));
-	// }
-	// httpGet.releaseConnection();
-	// _logger.info("Getting the Item with URL:"+fileURL+" with file
-	// name:"+fileSavePath+" Success");
-	// return isSucceed;
-	// }
-	private boolean getItems(String item, String fileSavePath) {
-		boolean isSucceed = true;
-		ByteArrayOutputStream baos = null;
-		ObjectOutputStream oos = null;
-		_logger.info("Getting the Item with  with file name:" + fileSavePath + " Started");
-		try {
-			baos = new ByteArrayOutputStream();
-			oos = new ObjectOutputStream(baos);
-			oos.writeObject(itemController.getItem(item).getEntity());
-			FileUtils.copyInputStreamToFile(new ByteArrayInputStream(baos.toByteArray()), new File(fileSavePath));
-			_logger.info("Getting the Item with  with file name:" + fileSavePath + " Success");
-			oos.flush();
-			oos.close();
-		} catch (Exception e) {
-			_logger.error("IO Exception occurred while Getting the Item " + e.getMessage());
-			isSucceed = false;
-			throw new ContentException(
-					String.format("UnKnown Exception Occurred while getting item ID: %s", e.getMessage()));
-		}
-		return isSucceed;
-	}
+	 private boolean getItems(URL fileURL, String fileSavePath) {
+	 _logger.info("Getting the Item with URL:"+fileURL+" with file name:"+fileSavePath+" Started");
+	 boolean isSucceed = true;
+	 CloseableHttpClient httpClient = HttpClients.createDefault();
+	 HttpGet httpGet = new HttpGet(fileURL.toString());
+	 httpGet.addHeader(USER_AGENT, USER_AGENT_VALUE);
+	 httpGet.addHeader(REFERRER, REFERRER_VALUE);
+	 try {
+	 CloseableHttpResponse httpResponse = httpClient.execute(httpGet);
+	 HttpEntity fileEntity = httpResponse.getEntity();
+	 if (fileEntity != null) {
+	 FileUtils.copyInputStreamToFile(fileEntity.getContent(), new
+	 File(fileSavePath));
+	 }
+	 } catch (IOException e) {
+	 _logger.error("IO Exception occurred while Getting the Item with URL:"+fileURL+ e.getMessage());
+	 throw new ContentException(String.format("UnKnown Exception Occurred while getting item ID: %s",e.getMessage()));
+	 }
+	 httpGet.releaseConnection();
+	 _logger.info("Getting the Item with URL:"+fileURL+" with file name:"+fileSavePath+" Success");
+	 return isSucceed;
+	 }
+	
 
 	public boolean unzip(String zipFilePath, String destDirectory) throws IOException {
 		_logger.info("Unzipping the File:" + zipFilePath + " to Location:" + destDirectory + " Started");
@@ -167,7 +140,7 @@ public class ItemViewerContentBuilder implements IContentBuilder {
 		} else {
 			_logger.error("Downloaed File:" + zipFilePath + " to Location:" + destDirectory
 					+ " is corrupted.make sure Item Exists");
-	//		new File(zipFilePath).delete();
+			new File(zipFilePath).delete();
 			isSucceed = false;
 			return isSucceed;
 		}
@@ -190,7 +163,7 @@ public class ItemViewerContentBuilder implements IContentBuilder {
 
 		try {
 			DESTINATION_ZIP_FILE_LOCATION = AppSettingsHelper.get("iris.ZipFileLocation");
-			// GIT_LAB_URL = AppSettingsHelper.get("iris.GitLabItemUrl");
+			 GIT_LAB_URL = AppSettingsHelper.get("iris.GitLabItemUrl");
 		} catch (Exception exp) {
 			_logger.error("Error loading zip file location or git lab url", exp);
 			throw new ContentException(exp);
