0483b614c9db372b91f7a73f4a7ab4a8c48df935
KThotti <khaleelthotti@gmail.com>
2016-08-11 20:47:08 -0700

    display commits as item versions

--- src/main/java/tds/iris/web/handlers/VersionsBacking.java
+++ src/main/java/tds/iris/web/handlers/VersionsBacking.java
@@ -9,6 +9,7 @@ import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
@@ -31,8 +32,11 @@ import tds.itemselection.base.ItemCandidatesData;
 public class VersionsBacking {
 	
 	private List<ItemVersion> itemVersions;
+
 	
-	//http://localhost:8090/webapi/api/item/version/item-187-1167
+	private List<ItemCommit> itemCommits;
+
+	//http://localhost:8090/webapi/api/item/commits/item-187-1167
 	private String gitLabUrl;
 
 	//http://localhost:8090/iris/IrisPages/sample.xhtml
@@ -87,6 +91,47 @@ public class VersionsBacking {
 		}
 		
 	}
+	private void commitsInit() {
+		try {
+			String type = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("type");
+			String bankId = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("bankId");
+			String itemNumber = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("id");
+
+			String id = type+ "-" + bankId + "-" + itemNumber;
+			
+			String queryString = String.format("?type=%s&bankId=%s&id=%s", type, bankId, itemNumber) ;
+			
+			gitLabUrl = gitLabUrl + "/" + id;
+			
+			URL url = new URL(gitLabUrl);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+			
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("Failed to connect to GitLab: HTTP error code : "
+						+ conn.getResponseCode());
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+			itemCommits = getItemCommits(br);
+			
+			for (ItemCommit itemCommit : itemCommits) {
+				itemCommit.setLink(irisPage +  queryString + "&version=" + itemCommit.getCommitId());
+			}
+
+			br.close();
+			conn.disconnect();
+			
+
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+	}
+
 	
 	private List<ItemVersion> getItemVersions(Reader reader) {
 
@@ -124,6 +169,59 @@ public class VersionsBacking {
 
 	}
 
+	private List<ItemCommit> getItemCommits(Reader reader) {
+
+		List<ItemCommit> itemCommits_ = new ArrayList<ItemCommit>();
+
+		try {
+			
+			JSONParser jsonParser = new JSONParser();
+			JSONArray names = (JSONArray) jsonParser.parse(reader);
+
+			Iterator i = names.iterator();
+			
+			// take each value from the json array separately
+			int count = names.size();
+			while (i.hasNext()) {
+				ItemCommit itemCommit = new ItemCommit();
+				JSONObject innerObj = (JSONObject) i.next();				
+				
+				/*
+				private String id;
+				private String shortId;
+				private String title;
+				private String authorName;
+				private String authorEmail;
+				private Date creationDate;
+				private String message;
+				*/
+				
+				itemCommit.setId(count--);
+				itemCommit.setCommitId((String)innerObj.get("id"));
+				itemCommit.setTitle((String)innerObj.get("title"));
+				itemCommit.setShortId((String)innerObj.get("short_id"));
+				itemCommit.setAuthorName((String)innerObj.get("author_name"));
+				itemCommit.setAuthorEmail((String)innerObj.get("author_email"));
+				
+				//String creationDateStr = (String)innerObj.get("created_at");
+				
+				
+				//itemCommit.setCreationDate((String)innerObj.get("created_at"));
+				itemCommit.setMessage((String)innerObj.get("message"));
+
+				itemCommits_.add(itemCommit);
+
+			}
+
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return itemCommits_;
+
+	}
+
+	
 	public List<ItemVersion> getItemVersions() {
 		versionsInit();
 		return itemVersions;
@@ -151,6 +249,19 @@ public class VersionsBacking {
 
 	public void setIrisPage(String irisPage) {
 		this.irisPage = irisPage;
+	}
+
+	public List<ItemCommit> getItemCommits() {
+		commitsInit();;
+		return itemCommits;
+	}
+
+	public void setItemCommits(List<ItemCommit> itemCommits) {
+		this.itemCommits = itemCommits;
+	}
+
+	public void setItemVersions(List<ItemVersion> itemVersions) {
+		this.itemVersions = itemVersions;
 	}
 	
 	
