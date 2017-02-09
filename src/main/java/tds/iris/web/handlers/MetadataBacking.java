4e71d42d9ea62731e76e02842ef8ee90baca92fa
suryasrinivas <suryasrinivas2000@gmail.com>
2017-02-08 16:42:08 -0800

    added all the fields in metadata

--- src/main/java/tds/iris/web/handlers/MetadataBacking.java
+++ src/main/java/tds/iris/web/handlers/MetadataBacking.java
@@ -1,7 +1,7 @@
 package tds.iris.web.handlers;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,53 +16,50 @@ import tds.iris.data.MetaData;
  * @author ssuryadevara
  *
  */
-@ManagedBean(name="metadataBacking")
-@Scope(value="request")
+@ManagedBean(name = "metadataBacking")
+@Scope(value = "request")
 public class MetadataBacking {
-	  private ItemViewerContentBuilder      contentBuilder;
-	 private static final Logger _logger = LoggerFactory.getLogger (MetadataBacking.class);
+	private ItemViewerContentBuilder contentBuilder;
+	private static final Logger _logger = LoggerFactory.getLogger(MetadataBacking.class);
+	private MetaData metadata = null;
 
 	public MetadataBacking() {
 		init();
-		
-	}
-	
-	
 
-	
+	}
+
 	private void init() {
-		contentBuilder = SpringApplicationContext.getBean ("iContentBuilder", ItemViewerContentBuilder.class);
-		
+		contentBuilder = SpringApplicationContext.getBean("iContentBuilder", ItemViewerContentBuilder.class);
+
 	}
 
+	public MetaData getMetadata() {
+		if (metadata == null) {
+			try {
+				String type = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap()
+						.get("type");
+				String bankId = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap()
+						.get("bankId");
+				String itemNumber = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap()
+						.get("id");
+				StringBuffer id = new StringBuffer();
+				if (StringUtils.hasLength(type) && StringUtils.hasLength(bankId) && StringUtils.hasLength(itemNumber)) {
 
+					id.append(type + "-" + bankId + "-" + itemNumber + "-");
+				}
+				String version = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap()
+						.get("version");
+				if (StringUtils.hasLength(version))
+					id.append(version);
 
+				metadata = contentBuilder.getMetadata(id.toString());
 
-	public MetaData getMetadata(){
-		try{
-		String type = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("type");
-		String bankId = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("bankId");
-		String itemNumber = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("id");
-		StringBuffer id = new StringBuffer() ;
-		if(StringUtils.hasLength(type) && StringUtils.hasLength(bankId) && StringUtils.hasLength(itemNumber)) {
-			
-			 id.append( type+ "-" + bankId + "-" + itemNumber+"-");
+			} catch (Exception e) {
+				_logger.error("exception occcurred while getting metadata", e);
+				return null;
+			}
 		}
-		String version = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("version");
-		if (StringUtils.hasLength(version))
-			id.append(version);
-		
-		return contentBuilder.getMetadata(id.toString());
-		}catch(Exception e){
-			_logger.error("exception occcurred while getting metadata", e);
-			return null;
-		}
-		
+		return metadata;
 	}
 
-
-
-
-
-	
 }
