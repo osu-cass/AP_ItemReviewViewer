ad4c62c067d0dbab337a9944dd9de0d190209bc8
KThotti <khaleelthotti@gmail.com>
2017-03-28 11:27:37 -0700

    created ItemScoreController

--- web/src/main/java/org/smarterbalanced/irv/web/controllers/RenderItemController.java
+++ web/src/main/java/org/smarterbalanced/irv/web/controllers/RenderItemController.java
@@ -1,30 +1,16 @@
 package org.smarterbalanced.irv.web.controllers;
 
-import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.smarterbalanced.irv.core.model.ItemRequestModel;
-import org.smarterbalanced.irv.model.ItemScoreInfo;
-import org.smarterbalanced.irv.services.ItemReviewScoringService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import AIR.Common.Utilities.SpringApplicationContext;
-import tds.iris.abstractions.repository.ContentException;
-import tds.iris.abstractions.repository.IContentBuilder;
-import tds.itemrenderer.data.IITSDocument;
 
 
 
@@ -36,18 +22,7 @@ import tds.itemrenderer.data.IITSDocument;
 public class RenderItemController {
 	
 	
-	  @Autowired
-	  private ItemReviewScoringService itemReviewScoringService;
-	  
-	  private IContentBuilder     _contentBuilder;
-	  
 	  private static final Logger _logger = LoggerFactory.getLogger (RenderItemController.class);
-
-	  @PostConstruct
-	  public synchronized void init () throws ContentException {
-	    _contentBuilder = SpringApplicationContext.getBean ("iContentBuilder", IContentBuilder.class);
-	  }
-
 
 	@RequestMapping(value = "/{itemId}", method = RequestMethod.GET)
 	@ResponseBody
@@ -67,33 +42,4 @@ public class RenderItemController {
 	}
 
 	
-	@RequestMapping(value="/score/{itemId}", method = RequestMethod.POST)
-	@ResponseBody
-	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String scoreItem(@PathVariable("itemId") String itemId,
-    						@RequestParam(value = "version", required = false, defaultValue = "") String version,
-    						@RequestBody String studentResponse) {
-
-		try {
-			String qualifiedItemId = "";
-			
-			if(version!=null && version.trim().length()>0)
-				qualifiedItemId = "I-" + itemId + "-" + version;
-			else	
-				qualifiedItemId = "I-" + itemId;
-			
-			IITSDocument iitsDocument =  _contentBuilder.getITSDocument(qualifiedItemId);
-			ItemScoreInfo itemScoreInfo = itemReviewScoringService.scoreAssessmentItem(studentResponse, iitsDocument);
-
-			ObjectMapper objectMapper = new ObjectMapper();
-			String jsonString = objectMapper.writeValueAsString(itemScoreInfo);
-			return jsonString;
-
-		} catch (Exception e) {
-			// TODO: handle exception
-			_logger.info("There is an error while scoring the item: " + itemId);
-			throw new ScoringException(e.getMessage(), itemId);
-		}
-		
-    }
 }
