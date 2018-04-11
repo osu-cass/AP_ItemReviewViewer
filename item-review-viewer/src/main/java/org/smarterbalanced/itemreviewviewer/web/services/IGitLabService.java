3f29f0ac8fc604a9e57a2438301658874f1216c0
Daniel Van Horn <vanhornd@oregonstate.edu>
2018-04-11 15:08:40 -0700

    update sb-components version

--- item-review-viewer/src/main/java/org/smarterbalanced/itemreviewviewer/web/services/IGitLabService.java
+++ item-review-viewer/src/main/java/org/smarterbalanced/itemreviewviewer/web/services/IGitLabService.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.util.List;
 
 import org.smarterbalanced.itemreviewviewer.web.models.ItemCommit;
+import org.smarterbalanced.itemreviewviewer.web.models.ItemDocument;
+import org.smarterbalanced.itemreviewviewer.web.models.ItemMetadataModel;
 import org.smarterbalanced.itemreviewviewer.web.models.Metadata;
 import tds.itemrenderer.data.IITSDocument;
 
@@ -11,14 +13,19 @@ public interface IGitLabService {
 
     boolean downloadItem(String itemNumber) throws GitLabException;
 
-    Metadata getMetadata(String itemNumber) throws GitLabException;
-
-    void downloadAssociatedItems(IITSDocument doc);
-
     boolean isItemExistsLocally(String itemNumber);
 
     String unzip(String itemNumber) throws IOException;
 
+    ItemDocument getRubrics(String itemName);
+
+    List<ItemCommit> getItemCommits(String itemName) throws GitLabException;
+
     List<ItemCommit> getItemCommits(String type, String bankId, String itemNumber) throws GitLabException;
 
+    Metadata getMetadata(String itemNumber) throws GitLabException;
+
+    ItemMetadataModel getItemMetadata(String itemId, String section) throws GitLabException;
+
+    void downloadAssociatedItems(IITSDocument doc);
 }
