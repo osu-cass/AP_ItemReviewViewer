03a0fd06f3f34387e8c6dbf83067fed2f6b9c28d
Ingyu Woo <iglenwoo@gmail.com>
2018-10-05 09:36:51 -0700

    style(code style): reformat and remove unused lines

--- item-review-viewer/src/main/java/org/smarterbalanced/itemreviewviewer/web/controllers/RenderItemController.java
+++ item-review-viewer/src/main/java/org/smarterbalanced/itemreviewviewer/web/controllers/RenderItemController.java
@@ -28,7 +28,6 @@ import tds.blackbox.ContentRequestException;
 import tds.irisshared.content.ContentException;
 import tds.irisshared.repository.IContentBuilder;
 import tds.itemrenderer.data.IITSDocument;
-import org.smarterbalanced.itemreviewviewer.web.services.GitLabUtils;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
@@ -66,12 +65,12 @@ public class RenderItemController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     @ResponseBody
     public ResponseEntity<String> getMetadata(
-                             @RequestParam(value = "namespace") String namespace,
-                             @RequestParam(value = "bankKey", required = false, defaultValue = "") String bankKey,
-                             @RequestParam(value = "itemKey") String itemKey,
-                             @RequestParam(value = "section", required = false, defaultValue = "") String section,
-                             @RequestParam(value = "revision", required = false, defaultValue = "") String revision,
-                             @RequestParam(value = "isaap", required = false, defaultValue = "") String isaapCodes
+            @RequestParam(value = "namespace") String namespace,
+            @RequestParam(value = "bankKey", required = false, defaultValue = "") String bankKey,
+            @RequestParam(value = "itemKey") String itemKey,
+            @RequestParam(value = "section", required = false, defaultValue = "") String section,
+            @RequestParam(value = "revision", required = false, defaultValue = "") String revision,
+            @RequestParam(value = "isaap", required = false, defaultValue = "") String isaapCodes
     ) throws ContentRequestException {
         if (StringUtils.isEmpty(bankKey)) bankKey = _getBankKeyByNamespace(namespace);
         String itemId = GitLabUtils.makeDirId(bankKey, itemKey);
@@ -87,8 +86,8 @@ public class RenderItemController {
             meta = _gitLabService.getItemMetadata(namespace, itemId, section);
             json = mapper.writeValueAsString(meta);
         } catch (Exception e) {
-            String err = "Item ("+ itemId +") not found.";
-            return new ResponseEntity<>(err,HttpStatus.NOT_FOUND);
+            String err = "Item (" + itemId + ") not found.";
+            return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(json, HttpStatus.OK);
     }
@@ -96,12 +95,12 @@ public class RenderItemController {
     @RequestMapping(value = "revisions", method = RequestMethod.GET)
     @ResponseBody
     public ResponseEntity<String> getItemRevisions(
-                                   @RequestParam(value = "namespace") String namespace,
-                                   @RequestParam(value = "bankKey", required = false, defaultValue = "") String bankKey,
-                                   @RequestParam(value = "itemKey") String itemKey,
-                                   @RequestParam(value = "section", required = false, defaultValue = "") String section,
-                                   @RequestParam(value = "isaap", required = false, defaultValue = "") String isaapCodes
-    ){
+            @RequestParam(value = "namespace") String namespace,
+            @RequestParam(value = "bankKey", required = false, defaultValue = "") String bankKey,
+            @RequestParam(value = "itemKey") String itemKey,
+            @RequestParam(value = "section", required = false, defaultValue = "") String section,
+            @RequestParam(value = "isaap", required = false, defaultValue = "") String isaapCodes
+    ) {
         if (StringUtils.isEmpty(bankKey)) {
             bankKey = _getBankKeyByNamespace(namespace);
         }
@@ -113,15 +112,16 @@ public class RenderItemController {
         List<ItemCommit> commits;
         List<RevisionModel> revisions;
 
-        try{
+        try {
             commits = _gitLabService.getItemCommits(namespace, itemId);
             revisions = new ArrayList<>();
-            for(ItemCommit commit: commits){
-                revisions.add(new RevisionModel(commit.getAuthorName(), commit.getCreationDate().getTime(), commit.getMessage(), commit.getId(), false));
+            for (ItemCommit commit : commits) {
+                revisions.add(new RevisionModel(commit.getAuthorName(), commit.getCreationDate().getTime(),
+                        commit.getMessage(), commit.getId(), false));
             }
             json = mapper.writeValueAsString(revisions);
-        }catch(JsonProcessingException e){
-            String err = "Item revisions for ("+ itemId +") not found.";
+        } catch (JsonProcessingException e) {
+            String err = "Item revisions for (" + itemId + ") not found.";
             return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(json, HttpStatus.OK);
@@ -158,39 +158,36 @@ public class RenderItemController {
 
     @RequestMapping(value = "banksections", method = RequestMethod.GET)
     @ResponseBody
-    public ResponseEntity<String> getSections(){
+    public ResponseEntity<String> getSections() {
         List<SectionModel> sections = Arrays.asList(
                 new SectionModel("ELA", "ELA"),
                 new SectionModel("MATH", "MATH"),
-                new SectionModel("SIW","SIW"));
+                new SectionModel("SIW", "SIW"));
         ObjectMapper mapper = new ObjectMapper();
         String json;
-        try{
+        try {
             json = mapper.writeValueAsString(sections);
-        }catch(JsonProcessingException e){
+        } catch (JsonProcessingException e) {
             String err = "Bank sections not found.";
             return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(json, HttpStatus.OK);
     }
 
-    @RequestMapping(
-            value = {"/items"},
-            method = {RequestMethod.GET}
-    )
+    @RequestMapping(value = {"/items"}, method = {RequestMethod.GET})
     @ResponseBody
-    public ModelAndView getIvsContent(@RequestParam(value = "ids",required = true) String[] itemId,
-                                   @RequestParam(value = "revision", required = false, defaultValue = "") String revision,
-                                   @RequestParam(value = "section", required = false, defaultValue = "") String section,
-                                   @RequestParam(value = "scrollToId",required = false,defaultValue = "") String scrollToId,
-                                   @RequestParam(value = "isaap",required = false,defaultValue = "") String accommodationCodes,
-                                   @RequestParam(value = "readOnly",required = false,defaultValue = "false") boolean readOnly,
-                                   @RequestParam(value = "loadFrom",required = false,defaultValue = "/home/tomcat7/content/gitlab") String loadFrom)
-    {
+    public ModelAndView getIvsContent(
+            @RequestParam(value = "ids", required = true) String[] itemId,
+            @RequestParam(value = "revision", required = false, defaultValue = "") String revision,
+            @RequestParam(value = "section", required = false, defaultValue = "") String section,
+            @RequestParam(value = "scrollToId", required = false, defaultValue = "") String scrollToId,
+            @RequestParam(value = "isaap", required = false, defaultValue = "") String accommodationCodes,
+            @RequestParam(value = "readOnly", required = false, defaultValue = "false") boolean readOnly,
+            @RequestParam(value = "loadFrom", required = false, defaultValue = "/home/tomcat7/content/gitlab") String loadFrom) {
         HashSet<String> codeSet = new HashSet<>(Arrays.asList(accommodationCodes.split(";")));
         ArrayList<String> codes = new ArrayList<>(codeSet);
         ItemRequestModel item;
-        if(section.isEmpty() && revision.isEmpty()){
+        if (section.isEmpty() && revision.isEmpty()) {
             item = new ItemRequestModel(itemId, codes, loadFrom);
         } else {
             item = new ItemRequestModel(itemId, codes, section, revision, loadFrom);
@@ -201,10 +198,11 @@ public class RenderItemController {
         if (!scrollToId.equals("")) {
             try {
                 scrollToDivId = "QuestionNumber_" + scrollToId.split("-")[1];
-            } catch (IndexOutOfBoundsException var12){}
+            } catch (IndexOutOfBoundsException var12) {
+                // TODO: handle exception
+                _logger.error("Exception occurred", var12);
+            }
         }
-
-
 
         ModelAndView model = new ModelAndView();
         model.setViewName("item");
@@ -218,10 +216,10 @@ public class RenderItemController {
 
     @RequestMapping(value = "/score/{itemId}", method = RequestMethod.POST)
     @ResponseBody
-    public String scoreItem(@PathVariable("itemId") String itemId,
-                            @RequestParam(value = "version", required = false, defaultValue = "") String version,
-                            @RequestBody String studentResponse) {
-
+    public String scoreItem(
+            @PathVariable("itemId") String itemId,
+            @RequestParam(value = "version", required = false, defaultValue = "") String version,
+            @RequestBody String studentResponse) {
         try {
             String qualifiedItemId = GitLabUtils.makeQualifiedItemId(itemId, version);
 
@@ -229,21 +227,20 @@ public class RenderItemController {
             ItemScoreInfo itemScoreInfo = _itemReviewScoringService.scoreAssessmentItem(studentResponse, iitsDocument);
 
             ObjectMapper objectMapper = new ObjectMapper();
-            String jsonString = objectMapper.writeValueAsString(itemScoreInfo);
-            return jsonString;
+            return objectMapper.writeValueAsString(itemScoreInfo);
 
         } catch (Exception e) {
             // TODO: handle exception
             throw new ScoringException(e.getMessage(), itemId);
         }
-
     }
 
     @RequestMapping(value = "/{img}.png", method = RequestMethod.GET)
     @ResponseBody
-    public byte[] rubricImage(@PathVariable("img") String img,
-                              @RequestParam("itemId") String itemId,
-                              @RequestParam(value = "version", required = false, defaultValue = "") String version) throws Exception {
+    public byte[] rubricImage(
+            @PathVariable("img") String img,
+            @RequestParam("itemId") String itemId,
+            @RequestParam(value = "version", required = false, defaultValue = "") String version) throws Exception {
         try {
             String qualifiedItemId = GitLabUtils.makeQualifiedItemId(itemId, version);
             IITSDocument iitsDocument = _contentBuilder.getITSDocument(qualifiedItemId);
@@ -253,6 +250,4 @@ public class RenderItemController {
             throw new Exception();
         }
     }
-
-
 }
